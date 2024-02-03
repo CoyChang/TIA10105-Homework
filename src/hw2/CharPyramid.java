d4293de2a90a317aea6923fbce623a8512d0ead0
Coy <qsrbto11@gmail.com>
2024-02-04 00:10:18 +0800

    fist commit

--- src/hw2/CharPyramid.java
+++ src/hw2/CharPyramid.java
@@ -14,20 +14,29 @@ package hw2;
 public class CharPyramid {
 
 	public static void main(String[] args) {
-		char letter = 'A';
-		for(int i=letter ; i<=letter+5 ; i++) {	//將letter變數指定給i取得ASCII碼
-			for (int j=letter ; j<=i ; j++) {
-				System.out.print((char)i);
-			}		
+		for(char c='A' ; c <= 'F' ; c++) {	//method 1
+			for(char j = 'A' ; j <= c ; j++) {
+				System.out.print(c);
+			}
 			System.out.println();
-		}
-//		
-//		for(int i=1 ; i<=6 ; i++) {		//另一個方法
+		}	
+		
+//		char letter = 'A';	//method 2
+//		for(int i=letter ; i<=letter+5 ; i++) {	//將letter變數指定給i取得ASCII碼
+//			for (int j=letter ; j<=i ; j++) {
+//				System.out.print((char)i);
+//			}		
+//			System.out.println();
+//		}
+////		
+//		for(int i=1 ; i<=6 ; i++) {		//method 3
 //			for (int j=1 ; j<=i ; j++) {
 //				System.out.print(letter);
 //			}
 //			letter++;
 //			System.out.println();
 //		}
+		
+		
 	}
 }
