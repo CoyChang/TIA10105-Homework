ddead7e8305866498922fba3c95205a6c0475a3f
CoyChang <eric86kimo@gmail.com>
2024-03-19 22:43:38 +0800

    revise DateFormat.java

--- src/hw10/DateFormat.java
+++ src/hw10/DateFormat.java
@@ -1,5 +1,6 @@
 package hw10;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
@@ -21,16 +22,21 @@ public class DateFormat {
 				m = date.substring(4, 6);	//month
 				d = date.substring(6);	//day
 				
-				GregorianCalendar obj = new GregorianCalendar();
-				boolean isLeapYear =  obj.isLeapYear(Integer.parseInt(y));
+				GregorianCalendar calendar = new GregorianCalendar();
+				calendar.set(Calendar.YEAR, Integer.parseInt(y));
+				calendar.set(Calendar.MONTH, Integer.parseInt(m)-1);
 				
-				if(!isLeapYear && m.equals("02") && (Integer.parseInt(d) > 28)){
-					System.out.println(y + "年不為閏年，2月不能超過28天");
-					continue;
-				}
+				int maxDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);	//輸入月份最大天數		
+				boolean isLeapYear =  calendar.isLeapYear(Integer.parseInt(y));	 //輸入年份是否為閏年
 				
-				if(isLeapYear && m.equals("02") && (Integer.parseInt(d) > 29)) {					
-					System.out.println(y + "年為閏年，2月不能超過29天");	
+				
+				//雖然有正規表示法擋住非日期輸入的格式，但無法擋住超出該月天數的數字，
+				//所以利用取得月份的最大天數判斷，此方法同時也會幫忙判斷該年是否為閏年
+				if(Integer.parseInt(d) > maxDayOfMonth) {	
+					System.out.println("超出" + m + "月最大天數囉!!");
+					if(!isLeapYear && m.equals("02")) {
+						System.out.println(y + "不是閏年");
+					}					
 					continue;
 				}
 				
